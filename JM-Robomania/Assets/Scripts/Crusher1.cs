d6a215cbf9d5362efea152fda6510d6ae7bb7f71
Bye <markhamonca@codeninjas.com>
2024-08-04 14:47:44 -0400

    finished Robomania

---
+++ JM-Robomania/Assets/Scripts/Crusher1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher1 : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.position.x <= -8 || transform.position.x >= 8)
+        {
+            speed *= -1;
+        }
+
+        float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+        transform.position = newPosition;
+    }
+}
+
