d68c51aa987918fcc1847020d359222e193fe16c
Bye <markhamonca@codeninjas.com>
2024-08-11 15:37:17 -0400

    fINISHHED JUNGLE ESCAPPEEPEPEPEPED
    EDWV2WF2DWWDTWT

---
+++ JM-JungleEscape/Assets/PY/DetectFalsePlatforms.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+    public LayerMask Plateform;
+    void Update()
+    {
+
+        hit = Physics.Raycast(transform.position, transform.forward, 1.5f);
+        Debug.DrawRay(transform.position, transform.forward * 1.5f, Color.yellow);
+
+        if (hit == true)
+        {
+            Debug.LogWarning("Be careful!");
+        }
+        else
+        {
+            Debug.Log("All clear!");
+        }
+    }
+   
+}
