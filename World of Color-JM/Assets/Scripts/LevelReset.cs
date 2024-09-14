dd87fdfbe7d4b0779e176c3d289a8780d8a2bf92
CNMarkham <markhamonca@codeninjas.com>
2024-09-14 15:32:22 -0400

    Finished World of Colour and PY
    rf4r3r33rf34r3rfv

---
+++ World of Color-JM/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    public ParticleSystem trail;
+
+    public void GameOver()
+    {
+        
+        gameObject.SetActive(false);
+        Invoke("Reload", 1);
+      
+        explosion.transform.position = transform.position;
+        explosion.Play();
+    }
+    void Reload()
+    {
+        SceneManager.LoadScene(2);
+    }
+   
+
+    void Start()
+    {
+        explosion.Stop();
+    }
+    
+}
+
+
